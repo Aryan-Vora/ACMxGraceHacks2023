76aa3aaa34355e4d5bec7515aff34ed7e617dbab
Aryan Vora <voraaryan1@gmail.com>
2023-11-10 18:06:11 -0800

    auth frontend done
    needs backend integration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../static/Auth.css";
 import emailIcon from "../assets/mail.png";
 import passwordIcon from "../assets/key.png";
-import logoAndText from "../assets/logoAndText.png";
+import logoAndText from "../assets/LogoAndText.png";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,46 +15,48 @@ function Login() {
   };
 
   return (
-    <div className="login-form-container">
-      <img src={logoAndText} alt="Logo" className="logo" />
-      <form onSubmit={handleSignIn}>
-        <h1 className="title">Sign In</h1>
+    <div className="background">
+      <div className="login-form-container">
+        <img src={logoAndText} alt="Logo" className="login-logo" />
+        <form onSubmit={handleSignIn}>
+          <h1 className="title">Sign In</h1>
 
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={emailIcon} alt="Email" className="icon" />
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email Address"
-              required
-            />
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={emailIcon} alt="Email" className="icon" />
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email Address"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={passwordIcon} alt="Password" className="icon" />
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={passwordIcon} alt="Password" className="icon" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <Link to="/signup">Forgot Password?</Link>
+          <Link to="/signup">Forgot Password?</Link>
 
-        <button className="login-button" type="submit">
-          Sign In
-        </button>
-      </form>
-      <div className="login-links">
-        <p>Don&apos;t have an account yet? </p>
-        <Link to="/signup">Sign up</Link>
+          <button className="signup-button" type="submit">
+            Sign In
+          </button>
+        </form>
+        <div className="login-links">
+          <p>Don&apos;t have an account yet? </p>
+          <Link to="/signup">Sign up</Link>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../static/Auth.css";
 import emailIcon from "../assets/mail.png";
 import passwordIcon from "../assets/key.png";
-import logoAndText from "../assets/logoAndText.png";
+import logo from "../assets/logo.png";
 import userIcon from "../assets/user.png";
 function Signup() {
   const [email, setEmail] = useState("");
@@ -14,6 +14,10 @@ function Signup() {
 
   const handleSignUp = (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return; // Stop the form submission
+    }
     // Updated log to include agreeToTerms
     console.log(
       "Full Name:",
@@ -31,78 +35,80 @@ function Signup() {
   };
 
   return (
-    <div className="login-form-container">
-      <img src={logoAndText} alt="Logo" className="logo" />
-      <h1 className="title">Sign Up</h1>
-      <form onSubmit={handleSignUp}>
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={userIcon} alt="Name" className="icon" />
-            <input
-              type="text"
-              id="name"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              placeholder="Full Name"
-              required
-            />
+    <div className="background">
+      <div className="signup-form-container">
+        <img src={logo} alt="Logo" className="signup-logo" />
+        <h1 className="title">Sign Up</h1>
+        <form onSubmit={handleSignUp}>
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={userIcon} alt="Name" className="icon" />
+              <input
+                type="text"
+                id="name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                placeholder="Full Name"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={emailIcon} alt="Email" className="icon" />
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email Address"
-              required
-            />
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={emailIcon} alt="Email" className="icon" />
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email Address"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={passwordIcon} alt="Password" className="icon" />
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={passwordIcon} alt="Password" className="icon" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="input-icon-container">
-            <img src={passwordIcon} alt="Password" className="icon" />
-            <input
-              type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="Confirm Password"
-              required
-            />
+          <div className="form-group">
+            <div className="input-icon-container">
+              <img src={passwordIcon} alt="Password" className="icon" />
+              <input
+                type="password"
+                id="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
           </div>
+          <label>
+            <input
+              className="terms"
+              type="checkbox"
+              checked={agreeToTerms}
+              onChange={(e) => setAgreeToTerms(e.target.checked)}
+            />
+            Agree with <strong>Terms and Conditions</strong>
+          </label>
+          <button className="signup-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+        <div className="login-links">
+          <p>Already have an account? </p>
+          <Link to="/login">Log in</Link>
         </div>
-        <label>
-          <input
-            className="terms"
-            type="checkbox"
-            checked={agreeToTerms}
-            onChange={(e) => setAgreeToTerms(e.target.checked)}
-          />
-          Agree with <strong>Terms and Conditions</strong>
-        </label>
-        <button className="signup-button" type="submit">
-          Sign Up
-        </button>
-      </form>
-      <div className="login-links">
-        <p>Already have an account? </p>
-        <Link to="/login">Log in</Link>
       </div>
     </div>
   );
