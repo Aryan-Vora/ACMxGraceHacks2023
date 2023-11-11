d8eeea75b9be4c7fbbbe6100ff24699f823fc53c
Aryan Vora <voraaryan1@gmail.com>
2023-11-10 20:33:26 -0800

    addded ehr

--- frontend/src/pages/UploadEHR.jsx
+++ frontend/src/pages/UploadEHR.jsx
@@ -1,28 +1,112 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../static/UploadEHR.css";
+import { pdfjs } from "react-pdf";
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+import { Document, Page } from "react-pdf";
 function UploadEHR() {
-  const [message, setMessage] = useState("");
+  // State to store the selected file and its data URL
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileDataUrl, setFileDataUrl] = useState("");
+  // State to control the visibility of the confirmation modal
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [numPages, setNumPages] = useState(null);
 
-  useEffect(() => {
-    // Fetch the message from the backend when the component mounts
-    // Add name as a parameter to the post request
-    fetch("api/hello", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: "John" }),
-    })
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  // Handler for file input change
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/pdf") {
+      setSelectedFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        // Set the data URL to state
+        setFileDataUrl(reader.result);
+        setSelectedFile(file);
+        setIsModalVisible(true); // Show the modal when a file is selected
+      };
+      reader.readAsDataURL(file);
+    }
+    console.log(selectedFile);
+  };
+  const navigate = useNavigate(); // Hook to get navigate function
+
+  // Handler for confirming file upload
+  const handleConfirmUpload = () => {
+    // Implement file upload logic here
+    setIsModalVisible(false);
+    navigate("/upload-medication");
+  };
+
+  // Handler for canceling file upload
+  const handleCancelUpload = () => {
+    setSelectedFile(null);
+    setFileDataUrl("");
+    setIsModalVisible(false);
+  };
+
   return (
-    <div>
-      <h1>Upload EHR Page</h1>
-      <p>Message from backend: {message}</p>
-      <Link to="/">Go back Home</Link>
+    <div className="background">
+      <div className="upload-container">
+        <h1 className="title">Upload Medical Records</h1>
+        <div className="uploadEHR-container">
+          <label htmlFor="file-upload" className="custom-file-upload">
+            <div className="upload-icon"></div>
+            <div className="filename">
+              {selectedFile ? selectedFile.name : "Filename.pdf"}
+            </div>
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            accept="application/pdf"
+            onChange={handleFileChange}
+            style={{ display: "none" }} // Hide the actual file input
+          />
+        </div>
+        <div className="filetype">Acceptable File Types: PDF</div>
+
+        {selectedFile && (
+          <div className={`modal ${isModalVisible ? "visible" : ""}`}>
+            <h2>Is this the correct file?</h2>
+
+            <div className="modal-content">
+              {fileDataUrl && (
+                <div className="pdf-container">
+                  <Document
+                    file={selectedFile}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                    className="pdf-document"
+                  >
+                    {Array.from(new Array(numPages), (el, index) => (
+                      <Page
+                        key={`page_${index + 1}`}
+                        pageNumber={index + 1}
+                        renderTextLayer={false}
+                        renderAnnotationLayer={false}
+                        scale={0.5}
+                      />
+                    ))}
+                  </Document>
+                </div>
+              )}
+            </div>
+            <button className="cancel" onClick={handleCancelUpload}>
+              Cancel
+            </button>
+            <button className="confirm" onClick={handleConfirmUpload}>
+              Confirm
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
