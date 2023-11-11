58bcb4e4a47ca90d13363c1ca46dbf824efaf334
amrita-pasupuleti <130122797+amrita-pasupuleti@users.noreply.github.com>
2023-11-11 04:17:09 -0800

    temp upload image of medication

--- frontend/src/pages/UploadMedication.jsx
+++ frontend/src/pages/UploadMedication.jsx
@@ -1,28 +1,93 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../static/UploadMedication.css";
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
 
 function UploadMedication() {
-  const [message, setMessage] = useState("");
+  // State to store the selected file and its data URL
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileDataUrl, setFileDataUrl] = useState("");
+  // State to control the visibility of the confirmation modal
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [numPages, setNumPages] = useState(null);
 
-  useEffect(() => {
-    // Fetch the message from the backend when the component mounts
-    // Add name as a parameter to the post request
-    fetch("api/hello", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: "John" }),
-    })
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  // Handler for file input change
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFileDataUrl(reader.result); // Set the data URL
+        setIsModalVisible(true); // Show the modal when a file is selected
+      };
+      reader.readAsDataURL(file);
+      setSelectedFile(file); // Set selected file
+    }
+  };
+
+  // ...
+
+  const navigate = useNavigate(); // Hook to get navigate function
+
+  // Handler for confirming file upload
+  const handleConfirmUpload = () => {
+    // Implement file upload logic here
+    setIsModalVisible(false);
+    navigate("/final-analysis");
+  };
+
+  // Handler for canceling file upload
+  const handleCancelUpload = () => {
+    setSelectedFile(null);
+    setFileDataUrl("");
+    setIsModalVisible(false);
+  };
+
   return (
-    <div>
-      <h1>Upload Medication Page</h1>
-      <p>Message from backend: {message}</p>
-      <Link to="/">Go back Home</Link>
+    <div className="background">
+      <div className="upload-container">
+        <h1 className="EHR-title">Upload Medication</h1>
+        <div className="uploadEHR-container">
+          <label htmlFor="file-upload" className="custom-file-upload">
+            <div className="upload-icon"></div>
+            <div className="filename">
+              {selectedFile ? selectedFile.name : "Filename.jpeg/png"}
+            </div>
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={handleFileChange}
+            style={{ display: "none" }} // Hide the actual file input
+          />
+        </div>
+        <div className="filetype">Acceptable File Types: JPEG/PNG</div>
+
+        {selectedFile && (
+          <div className={`modal ${isModalVisible ? "visible" : ""}`}>
+            <h2>Is this the correct file?</h2>
+
+            <div className="modal-content">
+              {fileDataUrl && (
+                <div className="pdf-container">
+                  <img src={fileDataUrl} alt="Uploaded File Preview"></img>{" "}
+                  {/* Use fileDataUrl here */}
+                </div>
+              )}
+            </div>
+            <button className="cancel" onClick={handleCancelUpload}>
+              Cancel
+            </button>
+            <button className="confirm" onClick={handleConfirmUpload}>
+              Confirm
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
